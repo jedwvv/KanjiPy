22bcc91669ce2e851f0182757230c3ea52f8791d
Jedwin Villanueva <jedwvv@gmail.com>
2023-11-10 13:57:01 +1100

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # KanjiPy
-A simple Python script to learn Kanji at your own pace
+A simple Python script to learn Kanji at your own pace.  
+
+## Requirement:
+- Python>=3.9  
+
+## Who is this for?
+- Japanese learners like me who want to extensively test their kanji reading skills (perhaps to take the JLPT exam).
+- People who want to personalize a list of kanji to memorize, for example, to learn 100 new kanji words/phrases a week, or 10 kanji words/phrases a day!
+
+Simply run in a console that supports utf-8 encoding. To check if your console is supported run `import sys; sys.getdefaultencoding()` in your Python shell. If it prints `'utf-8'` then this program should work.  
+
+## To download this program and my database:
+- Run: `git clone https://github.com/jedwvv/KanjiPy.git`
+
+## To use the program:
+- Run: `python kanjipy.py`  
+
+## To change to your own personalised Kanji list:
+1. Make a json text file in the folder `'databases/'`.  
+2. In a dictionary format, enter your kanji entries as key, value pairs. 
+3. In the `kanjipy.py` script, change the `filedir` variable to store the directory of your new json file (Line 12). The location is commented near the top of the script for easy access.
+  
+See existing `databases/` folder for examples of these text files.  
+For convenience, you can also modify an existing database within the program by selecting the appropriate menu(s).  
+Make sure there are no duplicate kanji words/phrases!
+The main reason to make your own list would be if you want to memorize specific kanji words/phrases.
+
+## Notes:
+- I am in the process of learning the Kanji from the book:「日本語能力試験ターゲット1000 N1 漢字 」(ISBN: 4010924217). As I study the kanji (in order), I will update the uploaded kanji database, partitioning the 1000+ kanji characters into separate files.
+- In the future, I plan to add the kanji characters from 「日本語能力試験ターゲット1000 N2 漢字」(ISBN: 4010924225) which I have already completely studied prior to making this python script. This is for completeness to include most of the [常用漢字/Jōyō Kanji](https://www.kanshudo.com/collections/joyo_kanji), if not all. 
+- Since I am following these two books, I adopt the naming convention `N1_A_B.json` or `N2_A_B.json` for my kanji database where A and B denote the starting and ending character index from the corresponding N1 or N2 book. You may ignore these and make your own personalised database as explained above.
+- Once those 2000+ N1 and N2 characters are in the database, I will compile into one whole database.
+
+
---
+++ kanjipy.py
@@ -0,0 +1,161 @@
+import sys
+import json
+import random
+from os import system, name
+from time import sleep
+from datetime import datetime
+random.seed(datetime.now().timestamp())
+
+############################################################
+#### CHANGE DESIRED DATABASE FILE DIRECTORY UNDER HERE #####
+############################################################
+filedir = "databases/database1_100.json"
+
+class KanjiDataBase:
+    def __init__(self):
+        self.data = {}
+    
+    def add_kanjiPhrase( self, kanji: str, reading: str ):
+        self.data[kanji] = reading
+        
+    def remove_kanjiPhrase( self, kanji: str ):
+        self.data.pop( kanji, "" )
+    
+    def save_database( self, filedir: str):
+        with open( filedir, "w" ) as f:
+            json.dump( self.data, f, ensure_ascii=False , indent=2 )
+        
+    def load_database( self, filedir: str):
+        with open( filedir, "r" ) as f:
+            self.data = json.load( f )
+    
+    def get_size( self ):
+        return len( self.data )
+
+    def test_kanjireading( self, number_to_test = 1 ):
+        if number_to_test < len(self.data):
+            raise Exception("Not enough kanji in database for desired number of kanji to test. Add more kanji or lower test number requirement.")
+        test_kanji = random.sample( list(self.data.items()), number_to_test )
+        print("\nTry to enter the correct reading for the following kanji.")
+        for s, (kanji, reading) in enumerate(test_kanji):
+            while True:
+                user_reading = get_user_input_string("\t{}) {}: ".format(s, kanji))
+                if user_reading != reading:
+                    user_input = get_user_input_string("\nWould you like to try again? 'y' or 'n': ", valid_choices=["y", "n"])
+                    if user_input == "y":
+                        continue
+                    else:
+                        print("The correct reading for {} is {}".format(kanji, reading))
+                        print("Moving onto the next kanji.")
+                        break
+                else:
+                    print("That's correct!")
+                    break
+
+def main():   
+    #Start Program 
+    kanji_data = KanjiDataBase()
+    kanji_data.load_database(filedir)
+    clear()
+    print("\nWelcome to KanjiPy! A simple Python script to learn Kanji at your own pace.")
+    
+    break_main_loop = False
+    while not break_main_loop:
+        print("\n\tWhat would you like to do?")
+        print("\t1: Add Kanji to the database")
+        print("\t2: Remove Kanji from the database")
+        print("\t3: Test your reading of kanji from the current database")
+        print("\t4: Exit program")
+        user_choice = get_user_input_number("\nEnter number here: ", valid_choices=[1,2,3,4])
+        if user_choice == 1:
+            while True:
+                clear()
+                number_of_kanji = get_user_input_number("How many kanji would you like to add: ")
+                for n in range(1, number_of_kanji+1):
+                    kanji_to_add = get_user_input_string("{}) What kanji would you like to add: ".format(n))
+                    reading_to_add = get_user_input_string("{}) What is the reading of this kanji: ".format(n))
+                    print("{}) Your kanji and reading inputs are: {} ({}) ".format(n, kanji_to_add, reading_to_add))
+                    user_choice = get_user_input_string("{}) Are you sure you would like to add this to the database? Enter 'y' or 'n': ".format(n), valid_choices=["y", "n"])
+                    if user_choice == "y":
+                        kanji_data.add_kanjiPhrase(kanji_to_add, reading_to_add)
+                    else:
+                        print("Skipping this kanji.")
+                try_again = get_user_input_string("Would you like to add more kanji? Enter 'y' or 'n': ", valid_choices=["y", "n"])
+                if try_again == "n":
+                    clear()
+                    break
+        elif user_choice == 2:
+            while True:
+                clear()
+                number_of_kanji = get_user_input_number("How many kanji would you like to remove: ")
+                for n in range(1, number_of_kanji+1):
+                    kanji_to_remove = get_user_input_string("{}) What kanji would you like to remove: ".format(n))
+                    user_choice = get_user_input_string("{}) Are you sure you would like to remove {} from the database? Enter 'y' or 'n': ".format(n), valid_choices=["y", "n"])
+                    if user_choice == "y":
+                        try:
+                            kanji_data.remove_kanjiPhrase(kanji_to_remove)
+                        except:
+                            print("Nothing happened.")
+                try_again = get_user_input_string("Would you like to remove more kanji? Enter 'y' or 'n': ", valid_choices=["y", "n"])
+                if try_again == "n":
+                    clear()
+                    break
+        elif user_choice == 3:
+            while True:
+                number_of_kanji = get_user_input_number("How many kanji would you like to test your reading on: ")
+                if number_of_kanji < 0 or number_of_kanji > kanji_data.get_size():
+                    print("Please pick a number between 1 and {}".format(kanji_data.get_size()))
+                    continue
+                else:
+                    kanji_data.test_kanjireading( number_to_test=number_of_kanji)
+                    try_again = get_user_input_string("Would you like to test more kanji readings? Enter 'y' or 'n': ", valid_choices=["y", "n"])
+                    if try_again == "n":
+                        clear()
+                        break
+        else:
+            break_main_loop = True
+            clear()
+    print("Would you like to overwrite the saved database? (If you added new ones)")
+    save_newdatabase = get_user_input_string( "Enter 'y' or 'n': ", valid_choices=["y", "n"] )
+    if save_newdatabase:
+        kanji_data.save_database(filedir)
+    print("Thanks for using KanjiPy! Have a good day and good luck with your Kanji studies!")
+    sleep(0.5)
+                
+def get_user_input_number( prompt: str, valid_choices: list = [] ):
+    while True:
+        try:
+            user_input = int( input(prompt) )
+            if user_input in valid_choices or len(valid_choices) == 0:
+                return user_input
+            else:
+                raise Exception()
+        except:
+            if len(valid_choices) > 0:
+                print("Please enter a valid number from {}.".format(valid_choices))
+            else:
+                print("Please enter a valid number.")
+
+def get_user_input_string( prompt: str, valid_choices: list = [] ):
+    while True:
+        try:
+            user_input = input(prompt)
+            if user_input in valid_choices or len(valid_choices) == 0:
+                return user_input
+            else:
+                raise Exception()
+        except:
+            if len(valid_choices) > 0:
+                print("Please enter a valid answer from {}.".format(valid_choices))
+            else:
+                print("Please enter a valid answer.")
+
+#Command to clear terminal for easy reading
+def clear():
+    if name == 'nt':
+        _ = system('cls') #Windows command
+    else:
+        _ = system('clear') #Nux style command
+
+if __name__ == "__main__":
+    main()
